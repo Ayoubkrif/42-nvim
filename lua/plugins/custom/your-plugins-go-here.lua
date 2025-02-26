50599fc156ad3f2169a92e1cdb74913bc80900da
Ayoub Krifa <krifa.ayoub@yahoo.com>
2025-02-26 11:40:26 +0100

    copilot

--- lua/plugins/custom/your-plugins-go-here.lua
+++ lua/plugins/custom/your-plugins-go-here.lua
@@ -3,11 +3,20 @@
 --If you feel like this plugin is useful, feel free to git add commit push and make a pull request from your fork on GitHub. If there is a good, general use-case, I will merge it into the main branch :)
 ]]--
 
-
 return {
-	-- The naming convention is "GitHub Profile/GitHub Repo"
-	--example:
-	-- 'elkowar/yuck.vim'
-	'Ayoubkrif/42-nvim'
-		
+  -- The naming convention is "GitHub Profile/GitHub Repo"
+  --example:
+  -- 'elkowar/yuck.vim'
+  --'Ayoubkrif/42-nvim',
+
+  -- Add GitHub Copilot plugin
+  {
+    'github/copilot.vim',
+    event = 'InsertEnter', -- Load Copilot on insert mode
+    config = function()
+      -- Optional: Configure Copilot settings
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  },
 }
