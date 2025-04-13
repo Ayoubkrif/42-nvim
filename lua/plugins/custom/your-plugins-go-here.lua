6fd51bbebd8bb5ac9a379bc2237c2d2db214688e
Ayoub Krifa <aykrifa@paul-f5Br1s1.clusters.42paris.fr>
2025-04-13 13:12:37 +0200

    dashboard-update

--- lua/plugins/custom/your-plugins-go-here.lua
+++ lua/plugins/custom/your-plugins-go-here.lua
@@ -8,26 +8,4 @@ return {
   --example:
   -- 'elkowar/yuck.vim'
   'Ayoubkrif/42-nvim',
-
-  -- Add GitHub Copilot plugin
-  {
-    'github/copilot.vim',
-    event = 'InsertEnter', -- Load Copilot on insert mode
-    config = function()
-      -- Optional: Configure Copilot settings
-      vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end,
-  },
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.c,*.h",
-  callback = function()
-    vim.cmd([[
-      syntax match DebugMacro /\<DBG\s*\>/ containedin=ALL
-      highlight DebugMacro guifg=#5c6370 ctermfg=8
-    ]])
-  end,
-})
-
 }
